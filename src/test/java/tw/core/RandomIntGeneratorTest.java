3318544149d39c5dfaf920a675c98719662f5ae1
shuhan2 <2472520650@qq.com>
2018-05-09 10:04:57 +0800

    randomInputGenerator

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -4,10 +4,15 @@ package tw.core;
 
 
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 import tw.core.generator.RandomIntGenerator;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -18,26 +23,43 @@ import static org.junit.Assert.assertTrue;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class RandomIntGeneratorTest {
-    private RandomIntGenerator generator = new RandomIntGenerator();
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+    private RandomIntGenerator generator;
 
+    @Before
+    public final void before() {
+        generator = new RandomIntGenerator();
+    }
+    @Test
+    public void should_throw_Error_when_diggitmax_less_than_numbersofNeed(){
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Can't ask for more numbers than are available");
+        generator.generateNum(3,4);
+    }
     @Test
     public void shouldGenerateStringOfLengthFour() {
         String generatedNumber = generator.generateNum(9,4);
-        assertEquals(4, generatedNumber.length());
+        assertEquals(4, Arrays.stream(generatedNumber.split(" ")).distinct().count());
+    }
+    @Test
+    public void should_Generate_StringOfLengthFive() {
+        String generatedNumber = generator.generateNum(9,5);
+        assertEquals(5, Arrays.stream(generatedNumber.split(" ")).count());
     }
 
     @Test
     public void shouldGenerate4Digits() {
 
         String generatedNumber = generator.generateNum(9,4);
-        assertTrue(StringUtils.isNumeric(generatedNumber));
+        assertTrue(Arrays.stream(generatedNumber.split(" ")).allMatch(item ->  Integer.parseInt(item) < 9));
     }
 
     @Test
     public void shouldGenerateNotRepeat() {
-        String generatedNumber = generator.generateNum(4,4);
-        for (int i = 0; i < 4; i++) {
-            assertFalse(generatedNumber.substring(i + 1, 4).contains(generatedNumber.substring(i, i + 1)));
+        String generatedNumber = generator.generateNum(9,4);
+        for (int i = 0; i < 3; i++) {
+            assertFalse(generatedNumber.split(" ").toString().substring(i + 1, 4).contains(generatedNumber.substring(i, i + 1)));
         }
     }
 
